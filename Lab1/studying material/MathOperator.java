ae34c21a64c4db4d741256cf5964544c5b55bb9c
Steven Elyakamal <steven.elyakamal3@gmail.com>
2026-09-03 23:45:41 +0000

    Lab1 second commit

---
+++ Lab1/studying material/MathOperator.java
@@ -0,0 +1,13 @@
+public class MathOperator {
+    public static void main(String []args){
+        int x; // declare variable
+        x = 0; //assignment
+        System.out.println("x = " + x);
+        int y = 1; //dec + assign
+        System.out.println("y = " + y);
+        System.out.println("7/2 = " + 7/2);
+        System.out.println(" 7%2 = " + 7%2);
+
+    }
+    
+}
---
+++ Lab1/studying material/PrintTest.java
@@ -0,0 +1,9 @@
+public class PrintTest { // Class PrintTest body
+    public static void main(String []args){ // make indentation
+        System.out.println("PrintTest started.");
+        System.out.print("Second Line\n");
+        System.out.print("Third Line\n");
+        System.out.print("\n");
+    }
+
+} //end of class
