ae091aa3e18231b96332327db74474199ecb7389
Steven Elyakamal <steven.elyakamal3@gmail.com>
2026-09-04 00:28:42 +0000

    inputtest

--- Lab1/studying material/MathOperator.java
+++ Lab1/studying material/MathOperator.java
@@ -7,6 +7,18 @@ public class MathOperator {
         System.out.println("y = " + y);
         System.out.println("7/2 = " + 7/2);
         System.out.println(" 7%2 = " + 7%2);
+        int a = 1; 
+        long b = 200L;
+        long c = a + b;
+        
+        System.out.println("c = " + c);
+        int e = 2;
+        double f = 3.5;
+        double g = e + f;
+        System.out.println("g = " + g);
+        double doubleValue = 2.5;
+        int intValue = (int) doubleValue;
+        System.out.println("int value of 2.5" + intValue);
 
     }
     
