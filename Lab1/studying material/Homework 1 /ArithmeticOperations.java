d562e8dce012b13fbc4ed8177c4f8dd3a6854c24
Steven Elyakamal <steven.elyakamal3@gmail.com>
2026-09-06 16:30:29 +0000

    already done wtih primitives

---
+++ Lab1/studying material/Homework 1 /ArithmeticOperations.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArithmeticOperations {
+    public static void main(String []args){
+
+
+        System.out.println("Enter score 1: ");
+        Scanner input = new Scanner(System.in);
+        int score1;
+        score1 = input.nextInt();
+        System.out.println("Score 1:" + score1);
+        input.close();
+
+
+
+
+    }    
+}
