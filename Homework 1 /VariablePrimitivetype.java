ab7a418d61682b74ed18667542c72f3c1ea3e59e
Steven Elyakamal <steven.elyakamal3@gmail.com>
2026-09-08 16:08:19 +0000

    made some changes

--- Homework 1 /VariablePrimitivetype.java
+++ Homework 1 /VariablePrimitivetype.java
@@ -1,20 +1,20 @@
 
 
-public class VariablePrimitivetype {
-    public static void main(String []args){
-        int Age = 18;
-        double height = 5.8;
-        char grade = 'A';
-        double weight = 65.5;
-        boolean student = true;
-        int Numberofclasses = 6;
+public class VariablePrimitivetype { //step 1: Created the VariablePrimitivetype class
+    public static void main(String []args){ //step 2: created the public void main method to set up for writing the body of the program
+        int Age = 18;//step 3: declared variable Age as an integer variable and set it to the value 18
+        double height = 5.8;//step 4: declared the variable height as a double and set it to the value 5.8
+        char grade = 'A';//step 5: declared the variable grade as a character variable and set it to the character string value 'A'
+        double weight = 65.5; //step 6: declared the variable weight as a double and set it to the value 65.5
+        boolean student = true; //step 7: declared the variable student as a boolean variable and set it to the value true
+        int Numberofclasses = 6;//step 8: declared the variable Numberofclasses as an integer variable and set it to the value 6.
 
-        System.out.println("Age: " + Age);
-        System.out.println("Height: " + height);
-        System.out.println("Grade: " + grade);
-        System.out.println("Weight: " + weight);
-        System.out.println("Number of Classes: " + Numberofclasses);
-        System.out.println("Student: " + student);
+        System.out.println("Age: " + Age); //step 9: printed the output Age.
+        System.out.println("Height: " + height);//step 10: printed the output Height.
+        System.out.println("Grade: " + grade);//step 11: printed the output Grade.
+        System.out.println("Weight: " + weight); //step 12: printed the output weight.
+        System.out.println("Number of Classes: " + Numberofclasses); //step 13: printed the output Number of Classes.
+        System.out.println("Student: " + student);//step 14: printed the output Student
 
     }
 }
