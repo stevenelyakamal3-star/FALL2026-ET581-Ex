16b7ec1424a8dd66b5258784f4402793029c30c4
Steven Elyakamal <steven.elyakamal3@gmail.com>
2026-09-20 18:15:26 +0000

    all changes made

---
+++ Homework3/ProblemOneWhileLoop.java
@@ -0,0 +1,21 @@
+
+
+public class ProblemOneWhileLoop {
+    public static void main(String[] args){
+
+       int i = 1;
+
+       while (i<=1001){
+            System.out.println(i);
+            i++;
+
+
+
+       }
+
+
+
+
+    }
+    
+}
