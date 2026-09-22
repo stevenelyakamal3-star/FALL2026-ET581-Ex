7db811a505e572cc47ee2b657170e24971a840ff
Steven Elyakamal <steven.elyakamal3@gmail.com>
2026-09-22 15:16:55 +0000

    studying material done

--- Lab1/StudyingMaterial.java
+++ Lab1/StudyingMaterial.java
@@ -5,48 +5,28 @@ public class StudyingMaterial {
 
     public static void main(String args []){
 
-        System.out.println("Enter student information: ");
+        System.out.println("Input: ");
         Scanner input = new Scanner(System.in);
-        int age;
-        age = input.nextInt();
-        System.out.println("Age: " + age);
-        double height;
-        height = input.nextDouble();
-        System.out.println("Height: " + height);
-       
-        char grade; 
-        grade = input.next().charAt(0);
-        System.out.println("Grade: " + grade);
-        double weight;
-        weight = input.nextDouble();
-        System.out.println("Weight: " + weight);
-        int Number;
-        Number = input.nextInt();
-        System.out.println("Number of Classes: " + Number);
-        boolean student = true;
-        System.out.println("Student: " + student);
-        System.out.println("Enter test scores: ");
-        int numberone;
-        numberone = input.nextInt();
-        System.out.println("Score 1: " + numberone);
-        
-        int numbertwo;
-        numbertwo = input.nextInt();
-        System.out.println("Score 2: " + numbertwo);
-        
-        int numberthree;
-        numberthree = input.nextInt();
-        System.out.println("Score 3: " + numberthree);
-        double Average = (numberone + numbertwo +numberthree)/3.0;
-        System.out.println("Average: " + Average);
-        System.out.println("Enter a four-digit integer: ");
-        int number;
-        number = input.nextInt();
-        System.out.println("first digit: " + number/1000);
-        System.out.println("second digit: " + (number/100) % 10);
-        System.out.println("third digit: " + (number/10) % 10);
-        System.out.println("fourth digit: " + number % 10);
+        String word;
+        word = input.nextLine().toUpperCase();
 
+        int position = 0;
+
+        String result ="";
+        while(position<word.length()){
+            char currentcharacter = word.charAt(position);
+            int alphabetposition = currentcharacter - 'A';
+            int newposition = alphabetposition - 2;
+            if (newposition <0){
+                newposition = newposition + 26;
+            }
+            char newcharacter = (char)('A'+newposition);
+            result = result + newcharacter;
+            position++;
+
+
+        }
+        System.out.println("Output: " + result);
 
 
         
