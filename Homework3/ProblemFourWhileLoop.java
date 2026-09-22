a18e1df89549079dd8bae340d9c36a34c3e11260
Steven Elyakamal <steven.elyakamal3@gmail.com>
2026-09-22 01:19:01 +0000

    forgot the input.close in problem three for loop added that in

---
+++ Homework3/ProblemFourWhileLoop.java
@@ -0,0 +1,26 @@
+
+
+import java.util.Scanner;
+
+
+
+
+public class ProblemFourWhileLoop {
+    public static void main(String[] args){
+        System.out.println("Input: ");
+        Scanner input = new Scanner(System.in);
+        
+
+
+
+
+
+        input.close
+
+
+
+
+
+    }
+    
+}
--- Homework3/ProblemThreeForLoop.java
+++ Homework3/ProblemThreeForLoop.java
@@ -36,7 +36,7 @@ public class ProblemThreeForLoop {
 
         System.out.println("Output: " + result);
 
-
+        input.close();
 
 
 
