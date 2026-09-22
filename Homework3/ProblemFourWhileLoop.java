60b6ad86d013f90319ca8705a1eec946e50e2921
Steven Elyakamal <steven.elyakamal3@gmail.com>
2026-09-22 01:35:33 +0000

    problem four while loop done

--- Homework3/ProblemFourWhileLoop.java
+++ Homework3/ProblemFourWhileLoop.java
@@ -9,13 +9,34 @@ public class ProblemFourWhileLoop {
     public static void main(String[] args){
         System.out.println("Input: ");
         Scanner input = new Scanner(System.in);
-        
+        String word;
+        word = input.nextLine();
+
+        int i = 0;
+        boolean foundduplicate = false;
+        while(i < word.length() -1){
+            if (word.charAt(i) == word.charAt(i+1)){
+                foundduplicate = true;
+                break;
 
 
 
+            }
+            i++;
 
 
-        input.close
+
+        }
+
+        if (foundduplicate){
+            System.out.println("Has consecutive duplicate.");
+        }
+        else{
+            System.out.println("No consecutive duplicate.");
+        }
+
+
+        input.close();
 
 
 
